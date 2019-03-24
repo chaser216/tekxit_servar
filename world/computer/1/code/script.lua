1cb62811a4d7358fd40bc213d31f8668059d54fc
Cosmin Bianu <cosmin.chaser@gmail.com>
2019-03-24 21:17:23 +0200

    Initial Commit

---
+++ world/computer/1/code/script.lua
@@ -0,0 +1,11 @@
+--textutils.slowPrint("Hi Hello there streinger")
+
+var1 = "start "
+var2 = "end "
+var3 = "finish"
+
+print(var1..var2..var3)
+
+if var1 == 'start ' then
+    print("fuckayouuuu")
+end
